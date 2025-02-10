36d59a9d54d66983cf7110113e33bd6179fc865d
Diego Tavares <sdiego14599@gmail.com>
2025-02-10 13:24:27 -0300

    adicionando estudos sobre: Anatomia de classes

--- formacao-java/src/etapa1_sintaxe_basica/curso1_introducao/topico1_anatomia_de_classes/MinhaClasse.java
+++ formacao-java/src/etapa1_sintaxe_basica/curso1_introducao/topico1_anatomia_de_classes/MinhaClasse.java
@@ -1,8 +1,26 @@
+// Anotações do Topico: Anatomia de classes
+
 public class MinhaClasse {
 
     public static void main(String[] args) {
+        System.out.println("Ola, Mundo");
 
-        System.out.println("Olá, Mundo!");
+        // Final indica que nao pode ser alterada, nome todo em caixa alta.
+        final String BR = "Brasil";
+        System.out.println(BR);
+
+        // Estudadando metodos
+        String primeiroNome = "Diego";
+        String segundooNome = "Tavares";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundooNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    // Metodo
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return "Resultado do metodo " + primeiroNome.concat(" ").concat(segundoNome);
 
     }
 }
